ddb4a6efb274228fb4d0fa4b86e92393eadb9015
Flore GRONDIN <grondinflore@gmail.com>
2021-11-19 11:25:49 +0100

    Alimentation Shared + javadoc

---
+++ MenuisMiageShared/src/main/java/fr/miage/m2/menuismiageshared/Affaire.java
@@ -0,0 +1,266 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.miage.m2.menuismiageshared;
+
+/**
+ *
+ * @author Flo
+ * Classe qui permet de gérer un objet de type Affaire lorsqu'on souhaite MAJ une affaire créée par un CA
+ */
+public class Affaire {
+    private Long idAffaire;
+    private EtatAffaire etatAffaire;
+    private String prenomClient;
+    private String nomClient;
+    private String adresseClient;
+    private String mailClient;
+    private String telClient;
+    private String geolocalisationClient;
+    private Disponibilite rdvCommercial;
+    private Disponibilite rdvPose;
+    private Long refCommande;
+    private String cotesCommande;
+    private double montantCommande;
+
+    /**
+     * Constructeur permet au CA de déclarer un objet de type Affaire
+     * @param idAffaire
+     * @param prenomClient
+     * @param nomClient
+     * @param adresseClient
+     * @param mailClient
+     * @param telClient 
+     */
+    public Affaire(Long idAffaire, String prenomClient, String nomClient, String adresseClient, String mailClient, String telClient) {
+        this.idAffaire = idAffaire;
+        this.prenomClient = prenomClient;
+        this.nomClient = nomClient;
+        this.adresseClient = adresseClient;
+        this.mailClient = mailClient;
+        this.telClient = telClient;
+    }
+
+    /**
+     * Récupérer l'id d'une affaire
+     * @return l'id d'une affaire
+     */
+    public Long getIdAffaire() {
+        return idAffaire;
+    }
+
+    /**
+     * Mettre à jour l'id d'une affaire
+     * @param idAffaire 
+     */
+    public void setIdAffaire(Long idAffaire) {
+        this.idAffaire = idAffaire;
+    }
+
+    /**
+     * Récupérer l'état d'une affaire
+     * @return enum de type EtatAffaire
+     */
+    public EtatAffaire getEtatAffaire() {
+        return etatAffaire;
+    }
+
+    /**
+     * Mettre à jour l'état d'une affaire
+     * @param etatAffaire enum de type EtatAffaire
+     */
+    public void setEtatAffaire(EtatAffaire etatAffaire) {
+        this.etatAffaire = etatAffaire;
+    }
+
+    /**
+     * Récupérer le prénom du client
+     * @return 
+     */
+    public String getPrenomClient() {
+        return prenomClient;
+    }
+
+    /**
+     * Mettre à jour le prénom d'un client
+     * @param prenomClient 
+     */
+    public void setPrenomClient(String prenomClient) {
+        this.prenomClient = prenomClient;
+    }
+
+    /**
+     * Récupérer le nom d'un client
+     * @return 
+     */
+    public String getNomClient() {
+        return nomClient;
+    }
+
+    /**
+     * Mettre à jour le nom d'un client
+     * @param nomClient 
+     */
+    public void setNomClient(String nomClient) {
+        this.nomClient = nomClient;
+    }
+
+    /**
+     * Récupérer l'adresse d'un client
+     * @return 
+     */
+    public String getAdresseClient() {
+        return adresseClient;
+    }
+
+    /**
+     * Mettre à jour l'adresse d'un client
+     * @param adresseClient 
+     */
+    public void setAdresseClient(String adresseClient) {
+        this.adresseClient = adresseClient;
+    }
+
+    /**
+     * Récupérer le mail d'un client
+     * @return 
+     */
+    public String getMailClient() {
+        return mailClient;
+    }
+
+    /**
+     * Mettre à jour le mail d'un client
+     * @param mailClient 
+     */
+    public void setMailClient(String mailClient) {
+        this.mailClient = mailClient;
+    }
+
+    /**
+     * Récupérer le numéro de téléphone d'un client
+     * @return 
+     */
+    public String getTelClient() {
+        return telClient;
+    }
+
+    /**
+     * Mettre à jour le numéro de téléphone d'un client
+     * @param telClient 
+     */
+    public void setTelClient(String telClient) {
+        this.telClient = telClient;
+    }
+
+    /**
+     * Mettre à jour la géolocalisation d'un client
+     * @return Adresse pour les futurs RDV
+     */
+    public String getGeolocalisationClient() {
+        return geolocalisationClient;
+    }
+
+    /**
+     * Récupérer la géolocalisation d'un client 
+     * @param geolocalisationClient Adresse pour les futurs RDV
+     */
+    public void setGeolocalisationClient(String geolocalisationClient) {
+        this.geolocalisationClient = geolocalisationClient;
+    }
+
+    /**
+     * Récupérer le RDV commercial fixé avec un client
+     * @return objet de type Disponibilite
+     */
+    public Disponibilite getRdvCommercial() {
+        return rdvCommercial;
+    }
+
+    /**
+     * Mettre à jour le RDV commercial fixé avec un client
+     * @param rdvCommercial Objet de type Disponibilite
+     */
+    public void setRdvCommercial(Disponibilite rdvCommercial) {
+        this.rdvCommercial = rdvCommercial;
+    }
+
+    /**
+     * Récupérer le RDV d'une équipe de pose fixé avec un client
+     * @return Objet de type Disponibilite
+     */
+    public Disponibilite getRdvPose() {
+        return rdvPose;
+    }
+
+    /**
+     * Mettre à jour le RDV d'une équipe de pose fixé avec un client
+     * @param rdvPose Objet de type Disponibilite
+     */
+    public void setRdvPose(Disponibilite rdvPose) {
+        this.rdvPose = rdvPose;
+    }
+
+    /**
+     * Récupérer la référence d'une commande passée par un client
+     * @return 
+     */
+    public Long getRefCommande() {
+        return refCommande;
+    }
+
+    /**
+     * Mettre à jour la référence d'une commande passée par un client
+     * @param refCommande 
+     */
+    public void setRefCommande(Long refCommande) {
+        this.refCommande = refCommande;
+    }
+
+    /**
+     * Récupérer les côtes du produit commandé par un client
+     * @return 
+     */
+    public String getCotesCommande() {
+        return cotesCommande;
+    }
+
+    /**
+     * Mettre à jour les côtes du produit commandé par un client
+     * @param cotesCommande 
+     */
+    public void setCotesCommande(String cotesCommande) {
+        this.cotesCommande = cotesCommande;
+    }
+
+    /**
+     * Récupérer le montant de la commande passée par le client
+     * @return 
+     */
+    public double getMontantCommande() {
+        return montantCommande;
+    }
+
+    /**
+     * Mettre à jour le montant de la commande passée par le client
+     * @param montantCommande 
+     */
+    public void setMontantCommande(double montantCommande) {
+        this.montantCommande = montantCommande;
+    }
+    
+}
+
+/**
+ * Permet de déclarer de nouveaux états pour une affaire
+ * @author Flo
+ */
+enum EtatAffaire{
+    COMMANDEE,
+    EN_ATTENTE,
+    RECEPTIONNEE,
+    POSEE,
+    CLOTUREE
+}
---
+++ MenuisMiageShared/src/main/java/fr/miage/m2/menuismiageshared/Disponibilite.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.miage.m2.menuismiageshared;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ *
+ * @author Flo
+ * Classe qui permet de gérer un objet de type Disponibilité lorsqu'on souhaite MAJ un RDV commercial ou avec une équipe de pose
+ */
+public class Disponibilite {
+    private Long idDisponibilite;
+    private Long idCommercial;
+    private Long idEquipePose;
+    private boolean estDispo;
+    private Timestamp dateRdv;
+
+    /**
+     * Constructeur vide pour un objet de type Disponibilite
+     */
+    public Disponibilite() {
+    }
+    
+    /**
+     * Constructeur pour déclarer la disponibilité d'un commercial
+     * @param idCommercial
+     * @param dateRdv
+     * @return La disponibilité créée
+     */
+    public static Disponibilite disponibiliteCommercial(Long idCommercial, Timestamp dateRdv) {
+        Disponibilite d = new Disponibilite();
+        // Génération d'un id unique
+        d.setIdDisponibilite(Long.parseLong(UUID.randomUUID().toString()));
+        d.setIdCommercial(idCommercial);
+        d.setDateRdv(dateRdv);
+        d.setEstDispo(true);
+        return d;
+    }
+
+    /**
+     * Constructeur pour déclarer la disponibilité d'une équipe de pose
+     * @param idEquipePose
+     * @param dateRdv 
+     * @return La disponibilité créée
+     */
+    public static Disponibilite disponibilitePose(Long idEquipePose, Timestamp dateRdv) {
+        Disponibilite d = new Disponibilite();
+        // Génération d'un id unique
+        d.setIdDisponibilite(Long.parseLong(UUID.randomUUID().toString()));
+        d.setIdEquipePose(idEquipePose);
+        d.setDateRdv(dateRdv);
+        d.setEstDispo(true);
+        return d;
+    }
+
+    /**
+     * Récupérer l'id d'une disponibilité
+     * @return 
+     */
+    public Long getIdDisponibilite() {
+        return idDisponibilite;
+    }
+
+    /**
+     * Mettre à jour l'id d'une disponibilité
+     * @param idDisponibilite 
+     */
+    public void setIdDisponibilite(Long idDisponibilite) {
+        this.idDisponibilite = idDisponibilite;
+    }
+
+    /**
+     * Récupérer l'id d'un commercial
+     * @return 
+     */
+    public Long getIdCommercial() {
+        return idCommercial;
+    }
+
+    /**
+     * Mettre à jour l'id d'un commercial
+     * @param idCommercial 
+     */
+    public void setIdCommercial(Long idCommercial) {
+        this.idCommercial = idCommercial;
+    }
+
+    /**
+     * Récupérer l'id d'une équipe de pose
+     * @return 
+     */
+    public Long getIdEquipePose() {
+        return idEquipePose;
+    }
+
+    /**
+     * Mettre à jour l'id d'une équipe de pose
+     * @param idEquipePose 
+     */
+    public void setIdEquipePose(Long idEquipePose) {
+        this.idEquipePose = idEquipePose;
+    }
+
+    /**
+     * Retoune si la disponibilité est encore valide
+     * @return 
+     * Si true, la disponibilité n'a pas encore été prise par un CA
+     * Si false, la disponibilité n'est plus valide (prise par un CA)
+     */
+    public boolean isEstDispo() {
+        return estDispo;
+    }
+
+    /**
+     * Met à jour la validité de la disponibilité
+     * @param estDispo 
+     */
+    public void setEstDispo(boolean estDispo) {
+        this.estDispo = estDispo;
+    }
+
+    /**
+     * Récupérer la date d'un RDV
+     * @return 
+     */
+    public Timestamp getDateRdv() {
+        return dateRdv;
+    }
+
+    /**
+     * Met à jour la date d'un RDV
+     * @param dateRdv 
+     */
+    public void setDateRdv(Timestamp dateRdv) {
+        this.dateRdv = dateRdv;
+    }
+    
+    
+}
