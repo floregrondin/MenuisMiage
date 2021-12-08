2d12614e61ba232979c69124c04671c2cc8e4848
alexisBrnvd <alexis.bournavaud@toulouse.miage.fr>
2021-12-08 19:59:53 +0100

    JMS message

---
+++ AppliAchats/AppliAchats-ejb/src/main/java/fr/miage/m2/metier/GestionAchat.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.miage.m2.metier;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+/**
+ *
+ * @author Alexis Bournavaud
+ */
+@Stateless
+public class GestionAchat implements GestionAchatLocal {
+
+    @Resource(mappedName = "EtatCommande")
+    private Queue etatCommande;
+
+    @Resource(mappedName = "menuisMiage")
+    private ConnectionFactory menuisMiage;
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+    private Message createJMSMessageForetatCommande(Session session, Object messageData) throws JMSException {
+        // TODO create and populate message to send
+        TextMessage tm = session.createTextMessage();
+        tm.setText(messageData.toString());
+        return tm;
+    }
+
+    private void sendJMSMessageToEtatCommande(Object messageData) throws JMSException {
+        Connection connection = null;
+        Session session = null;
+        try {
+            connection = menuisMiage.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer messageProducer = session.createProducer(etatCommande);
+            messageProducer.send(createJMSMessageForetatCommande(session, messageData));
+        } finally {
+            if (session != null) {
+                try {
+                    session.close();
+                } catch (JMSException e) {
+                    Logger.getLogger(this.getClass().getName()).log(Level.WARNING, "Cannot close session", e);
+                }
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+    
+    
+}
